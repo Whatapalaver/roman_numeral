f91e44fd5607206471e3f213acbeb7527bae4061
Angela Wolff <warriorwomenblog@gmail.com>
2018-07-07 22:03:20 +0100

    Two passing tests first logic stage

--- lib/roman_decoder.rb
+++ lib/roman_decoder.rb
@@ -1,3 +1,9 @@
-def roman_decoder
-
+def roman_decoder(roman)
+  arabic = ""
+  3.times do
+    if roman.slice("I") != nil
+      arabic << roman.slice!("I")
+    end
+  end
+  return arabic.length
 end
--- spec/roman_decoder_spec.rb
+++ spec/roman_decoder_spec.rb
@@ -4,4 +4,7 @@ describe 'roman_decoder' do
   it 'returns 1 when passed I' do
     expect(roman_decoder("I")).to eq 1
   end
+  it 'returns 3 when passed III' do
+    expect(roman_decoder("III")).to eq 3
+  end
 end
