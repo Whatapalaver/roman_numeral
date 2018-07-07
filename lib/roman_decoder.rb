e5a6ed4d8824405b2e74bec73e512b1e76a8f914
Angela Wolff <warriorwomenblog@gmail.com>
2018-07-07 22:18:17 +0100

    Twelve passing tests but requires a bit of refactoring

--- lib/roman_decoder.rb
+++ lib/roman_decoder.rb
@@ -1,9 +1,54 @@
 def roman_decoder(roman)
-  arabic = ""
-  3.times do
+  arabic = 0
+  roman.length.times do
+    if roman.slice("CM") != nil
+      roman.slice!("CM")
+      arabic += 900
+    end
+    if roman.slice("CD") != nil
+      roman.slice!("CD")
+      arabic += 400
+    end
+    if roman.slice("XL") != nil
+      roman.slice!("XL")
+      arabic += 40
+    end
+    if roman.slice("IX") != nil
+      roman.slice!("IX")
+      arabic += 9
+    end
+    if roman.slice("IV") != nil
+      roman.slice!("IV")
+      arabic += 4
+    end
+    if roman.slice("M") != nil
+      roman.slice("M")
+      arabic += 1000
+    end
+    if roman.slice("D") != nil
+      roman.slice("D")
+      arabic += 500
+    end
+    if roman.slice("C") != nil
+      roman.slice("C")
+      arabic += 100
+    end
+    if roman.slice("L") != nil
+      roman.slice("L")
+      arabic += 50
+    end
+    if roman.slice("X") != nil
+      roman.slice("X")
+      arabic += 10
+    end
+    if roman.slice("V") != nil
+      roman.slice("V")
+      arabic += 5
+    end
     if roman.slice("I") != nil
-      arabic << roman.slice!("I")
+      roman.slice("I")
+      arabic += 1
     end
   end
-  return arabic.length
+  return arabic
 end
--- spec/roman_decoder_spec.rb
+++ spec/roman_decoder_spec.rb
@@ -1,10 +1,42 @@
 require 'roman_decoder'
 
 describe 'roman_decoder' do
+  it 'returns 900 when passed CM' do
+    expect(roman_decoder("CM")).to eq 900
+  end
+  it 'returns 400 when passed CD' do
+    expect(roman_decoder("CD")).to eq 400
+  end
+  it 'returns 40 when passed XL' do
+    expect(roman_decoder("XL")).to eq 40
+  end
+  it 'returns 9 when passed IX' do
+    expect(roman_decoder("IX")).to eq 9
+  end
+  it 'returns 1000 when passed M' do
+    expect(roman_decoder("M")).to eq 1000
+  end
+  it 'returns 500 when passed D' do
+    expect(roman_decoder("D")).to eq 500
+  end
+  it 'returns 100 when passed C' do
+    expect(roman_decoder("C")).to eq 100
+  end
+  it 'returns 50 when passed L' do
+    expect(roman_decoder("L")).to eq 50
+  end
+  it 'returns 10 when passed X' do
+    expect(roman_decoder("X")).to eq 10
+  end
+  it 'returns 5 when passed V' do
+    expect(roman_decoder("V")).to eq 5
+  end
   it 'returns 1 when passed I' do
     expect(roman_decoder("I")).to eq 1
   end
   it 'returns 3 when passed III' do
     expect(roman_decoder("III")).to eq 3
   end
+
+
 end
